0f81905dd5dcae1b2f85d4cfc9327d1b33b5275c
hongyang <hzha464@aucklanduni.ac.nz>
2025-01-17 01:18:12 +1300

    start-set

---
+++ front-end/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+import buttons from './uiStuff/uiButton'
+
+const joinRoom = ()=>{
+   const userName =document.getElementById('username').value
+   const roomName =document.getElementById('room-inut').value
+}
+buttons.joinRoom.addEventListener('click', joinRoom)
